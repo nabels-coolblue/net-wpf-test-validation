36051cf711dd9a48d8054e89d7a13b04d663464b
Niels Abels <n.abels@coolblue.nl>
2014-07-23 17:38:18 +0200

    added added domain object base class

---
+++ Wpf.Test.Domain/Common/DomainObject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace Wpf.Test.Domain.Common
+{
+    public class DomainObject : INotifyPropertyChanged
+    {
+        protected DomainObject() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            ValidateProperty(propertyName, value);
+
+            if (object.Equals(storage, value)) return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected void OnPropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            var propertyName = PropertySupport.ExtractPropertyName(propertyExpression);
+            this.OnPropertyChanged(propertyName);
+        }
+
+        protected void ValidateProperty(object value, [CallerMemberName] string propertyName = null)
+        {
+            ValidateProperty(propertyName, value);
+        }
+
+        protected virtual void ValidateProperty(string propertyName, object value)
+        {
+        }
+    }
+}
