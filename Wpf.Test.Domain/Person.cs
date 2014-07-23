44a211fc8c76b01728fbce25b5f1777aed11281f
Niels Abels <n.abels@coolblue.nl>
2014-07-23 17:58:14 +0200

    added person object

---
+++ Wpf.Test.Domain/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf.Test.Domain.Common;
+
+namespace Wpf.Test.Domain
+{
+    public class Person : DomainObject
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                SetProperty(ref _firstName, value);
+                OnPropertyChanged(() => FullName);
+            }
+        }
+
+        private string _surName;
+        public string SurName
+        {
+            get
+            {
+                return _surName;
+            }
+            set
+            {
+                SetProperty(ref _surName, value);
+                OnPropertyChanged(() => FullName);
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", FirstName, SurName);
+            }
+        }
+    }
+}
